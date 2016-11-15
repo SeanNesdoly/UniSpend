0ce89abf06325a9ff4b873ffccb8e8b6a8c11699
Patrick Gibson <patrickgibson@PataPro.local>
2016-11-15 11:32:32 -0500

    Updated transactions

---
+++ unispend/US_Transaction.cpp
@@ -0,0 +1,177 @@
+//
+// Created by Patrick Gibson on 2016-11-13.
+//
+
+#include "US_Transaction.h"
+US_Transaction::US_Transaction(string user, string name, string type, string value, string date, string recurring) {
+    this->User = user;
+    this->name = name;
+    this->type = type;
+    this->value = value;
+    this->date = date;
+    this->recurring = recurring;
+}
+
+void US_Transaction::setUser(const string user){
+    this->User = user;
+}
+
+void US_Transaction::setName(const string name){
+    this->name = name;
+}
+
+void US_Transaction::setType(const string type){
+    this->type = type;
+}
+
+void US_Transaction::setValue(const string value){
+    this->value = value;
+}
+
+void US_Transaction::setDate(const string date) {
+    this->date = date;
+}
+
+string US_Transaction::getUser(){
+    return User;
+}
+
+string US_Transaction::getName(){
+    return name;
+}
+
+string US_Transaction::getType(){
+    return type;
+}
+
+string US_Transaction::getValue(){
+    return value;
+}
+
+string US_Transaction::getDate(){
+   return date;
+}
+
+int US_Transaction::addTransaction() {
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+    driver = sql::mysql::get_mysql_driver_instance();
+    con = driver->connect("tcp://127.0.0.1:3306", "root", "");
+    stmt = con->createStatement();
+    stmt->execute("USE US_Database");
+    string sqlCommand =
+            "INSERT INTO `transactions` (`User`, `name`, `type`, `value`, `date`, `isRecurring`) VALUES ('" +User+ "','" + name + "','" + type + "','" + value + "','" + date + "','" + recurring + "')";
+    stmt->execute(sqlCommand);
+    delete con;
+    delete stmt;
+    return 0;
+}
+
+vector<US_Transaction> US_Transaction::getTransaction(const string user){
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+    sql::ResultSet *res;
+    driver = sql::mysql::get_mysql_driver_instance();
+    vector<US_Transaction> results;
+    con = driver->connect("tcp://127.0.0.1:3306", "root", "");
+    stmt = con->createStatement();
+    stmt->execute("USE US_Database");
+    res = stmt->executeQuery("SELECT * FROM  `transactions` WHERE `User` = '" +user+ "'");
+    while(res->next()){
+        US_Transaction* trans = new US_Transaction(res->getString("User"), res->getString("name"), res->getString("type"), res->getString("value")
+        ,res->getString("date"), res->getString("isRecurring"));
+        results.push_back(*trans);
+    }
+    delete res;
+    delete con;
+    delete stmt;
+    return results;
+
+}
+
+vector<US_Transaction> US_Transaction::getTransaction(const string user, const string date){
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+    sql::ResultSet *res;
+    driver = sql::mysql::get_mysql_driver_instance();
+    vector<US_Transaction> results;
+    con = driver->connect("tcp://127.0.0.1:3306", "root", "");
+    stmt = con->createStatement();
+    stmt->execute("USE US_Database");
+    res = stmt->executeQuery("SELECT * FROM  `transactions` WHERE `User` = '" +user+ "' AND `date` >= '"+date+ "' AND `date` < NOW() ");
+    while(res->next()){
+        US_Transaction* trans = new US_Transaction(res->getString("User"), res->getString("name"), res->getString("type"), res->getString("value")
+                ,res->getString("date"), res->getString("isRecurring"));
+        results.push_back(*trans);
+    }
+    delete res;
+    delete con;
+    delete stmt;
+    return results;
+}
+
+vector<US_Transaction> US_Transaction::getTransactionBetween(const string user, const string date1, const string date2){
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+    sql::ResultSet *res;
+    driver = sql::mysql::get_mysql_driver_instance();
+    vector<US_Transaction> results;
+    con = driver->connect("tcp://127.0.0.1:3306", "root", "");
+    stmt = con->createStatement();
+    stmt->execute("USE US_Database");
+    res = stmt->executeQuery("SELECT * FROM  `transactions` WHERE `date` > '" +date1+ "' AND `date` < '"+date2+ "' AND `User` = '"+user+"'");
+    while(res->next()){
+        US_Transaction* trans = new US_Transaction(res->getString("User"), res->getString("name"), res->getString("type"), res->getString("value")
+                ,res->getString("date"), res->getString("isRecurring"));
+        results.push_back(*trans);
+    }
+    delete res;
+    delete con;
+    delete stmt;
+    return results;
+}
+
+int main(){
+    cout << "Please enter the following:" << endl;
+    cout << "Enter the user: ";
+    string user;
+    cin >> user;
+    cout << "Name: ";
+    string name;
+    cin >> name;
+    string type;
+    cout << "Type: ";
+    cin >> type;
+    string value;
+    cout << "Value: ";
+    cin >> value;
+    string date;
+    cout << "Date (as YYYY-MM-DD) : ";
+    cin >> date;
+    string recurring;
+    cout << "Recurring? (1 or 0) : ";
+    cin >> recurring;
+    US_Transaction trans = US_Transaction(user, name, type, value, date, recurring);
+    trans.addTransaction();
+//    US_Transaction trans = US_Transaction("p", "q", "e", "f", "w");
+//    cout << "Name: ";
+//    string name2;
+//    cin >> name2;
+//    string date;
+//    cout << "Enter date: ";
+//    cin >> date;
+//    vector<US_Transaction> results = trans.getTransaction(name2, date);
+//    for(int i = 0; i < results.size(); i++){
+//        cout << results.at(i).getName() << results.at(i).getType() << results.at(i).getDate() << endl;
+//    }
+
+
+
+
+    return 0;
+}
+
