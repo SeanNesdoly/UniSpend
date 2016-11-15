b0b0194bc2b21355e2ccd7a79eaeb7ad59d0cf0e
srnesdoly <srnesdoly@me.com>
2016-11-15 17:17:08 -0500

    Added in menu items that will contain each tab container

--- unispend/US_Workspace.cpp
+++ unispend/US_Workspace.cpp
@@ -9,6 +9,8 @@
  */
 
 #include <Wt/WApplication>
+#include <Wt/WMenu>
+#include <Wt/WText>
 
 #include "US_Workspace.h"
 
@@ -20,8 +22,20 @@ US_Workspace::US_Workspace(WContainerWidget *parent):
     setStyleClass("workspace");
 
     // create the navigation bar
-    _navigation = new WNavigationBar();
+    _navigation = new WNavigationBar(parent);
     _navigation->setTitle("UniSpend");
+    _navigation->setResponsive(true);
     addWidget(_navigation);
 
+    // create the stacked widget that contains each tab
+    WStackedWidget *tabsStack = new WStackedWidget(parent);
+
+    WMenu *leftMenu = new WMenu(tabsStack, parent);
+    _navigation->addMenu(leftMenu);
+
+    //TODO: NAT replace WText (the 2nd arg) with your generated code classes
+    leftMenu->addItem("Overview", new WText("Overview"));
+    leftMenu->addItem("Transactions", new WText("Transactions"));
+    leftMenu->addItem("Forecasting", new WText("Forecasting"));
+    leftMenu->addItem("Settings", new WText("Settings"));
 }
