1c90e7e8d4aa3d3f708bd3f6d51c2d3e99b68449
Patrick Gibson <patrickgibson@PataPro.local>
2016-11-15 11:35:25 -0500

    Updated US_Transaction.h

--- unispend/US_Transaction.h
+++ unispend/US_Transaction.h
@@ -6,7 +6,6 @@
 #define TRANSACTIONS_US_TRANSACTION_H
 
 #include <string>
-//#include "boost/date_time/gregorian/gregorian.hpp"
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
@@ -14,24 +13,32 @@
 #include <cppconn/statement.h>
 #include "mysql_driver.h"
 #include "string.h"
+#include <vector>
 
 using namespace std;
 
 class US_Transaction{
 public:
-    US_Transaction(string name, string type, string value, string date, string recurring);
+    US_Transaction(string user, string name, string type, string value, string date, string recurring);
+    string getUser();
     string getName();
     string getType();
     string getValue();
-//    boost::gregorian::date getDate();
+    string getDate();
+    void setUser(const string user);
     void setName(const string name);
     void setType(const string type);
     void setValue(const string value);
-//    void setDate(const boost::gregorian::date date);
+    void setDate(const string date);
     int addTransaction();
+    vector<US_Transaction> getTransaction(const string user);
+    vector<US_Transaction> getTransaction(const string user, const string date);
+    vector<US_Transaction> getTransactionBetween(const string user, const string date1, const string date2);
+
 
 
 private:
+    string User;
     string name;
     string type;
     string value;
