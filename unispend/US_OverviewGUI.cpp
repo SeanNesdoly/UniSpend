ea8f5ca94b08e7b8be782c36d4bedcf4e6acd06b
Ryan.Fredrickson <13krf2@queensu.ca>
2016-11-23 00:14:29 -0500

    Overview files as of Wednesday 12:30 AM

--- unispend/US_OverviewGUI.cpp
+++ unispend/US_OverviewGUI.cpp
@@ -7,8 +7,28 @@
 US_OverviewGUI::US_OverviewGUI(WContainerWidget *parent):
         WContainerWidget(parent)
 {
+    //Set Style
     setStyleClass("overview");
-    
+    //Get current and start date
+    time_t t = time(0);   // get time now
+    struct tm * now = localtime( & t );
+    int currentDyear= now->tm_year+1900;
+    int currentDmonth= now->tm_mon+1;
+    int currentDday= now->tm_mday;
+    int startDday=currentDday;
+    if (currentDmonth==1){
+        int startDyear=currentDyear-1;
+        int startDmonth=12;
+    }
+    else{
+        int startDyear=currentDyear;
+        int startDmonth=currentDmonth;
+    }
 
+    //Show start and end date
+    startDate = new WLabel("Start Date:", this);
+    currentDate = new WLabel("End Date:", this);
+    addWidget(startDate);
+    addWidget(currentDate);
 
 }
--- unispend/US_OverviewGUI.h
+++ unispend/US_OverviewGUI.h
@@ -2,15 +2,17 @@
 // Created by Ryan Fredrickson on 2016-11-17.
 //
 #pragma once
+#include <ctime>
 #include <Wt/WContainerWidget>
+#include <Wt/WLabel>
 using namespace Wt;
 
 class US_OverviewGUI : public WContainerWidget{
 public:
     US_OverviewGUI(WContainerWidget *parent);
 private:
-
-
+    WLabel* startDate;
+    WLabel* currentDate;
 };
 
 
