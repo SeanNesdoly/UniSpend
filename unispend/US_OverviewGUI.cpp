a4d98a0fa93ba5c3bdbdbd8c7d05f602dfc71f03
Ryan.Fredrickson <13krf2@queensu.ca>
2016-11-29 17:50:10 -0500

    Scatter chart in

--- unispend/US_OverviewGUI.cpp
+++ unispend/US_OverviewGUI.cpp
@@ -73,29 +73,27 @@ US_OverviewGUI::US_OverviewGUI(US_Workspace *parent):
     int s=Transactions.size();
     vector<string> typeexpenses= {};
     vector<double> valueexp={};
+    vector<string> dates= {};
+    dates.push_back(stardate);
     model = new WStandardItemModel(this);
     //headers
     model->insertColumns(model->columnCount(), 2);
     model->setHeaderData(0, WString("Expense_Type"));
     model->setHeaderData(1, WString("Amount"));
     int rowData=0;
-    cout<<"I did it yeah:\n";
-    cout<<s;
     for(int i=0; i<s; i++){
         string type=Transactions.at(i).getType();
         double val=Transactions.at(i).getValue();
-        cout<<type;
+        string date=Transactions.at(i).getDate();
+        if (std::find(dates.begin(), dates.end(), date) == dates.end()){
+            dates.push_back(date);
+        }
         if (std::find(typeexpenses.begin(), typeexpenses.end(), type) != typeexpenses.end())
         {
-            cout<<"\nrepeat\n";
             int pos = std::find(typeexpenses.begin(), typeexpenses.end(), type) - typeexpenses.begin();
-            cout<<"position:";
-            cout<<pos;
             double sum=valueexp.at(pos);
             sum +=val;
             valueexp.at(pos)=sum;
-            cout<<"\n sum:";
-            cout<<sum;
         }
         else{
             rowData++;
@@ -103,12 +101,37 @@ US_OverviewGUI::US_OverviewGUI(US_Workspace *parent):
             valueexp.push_back(val);
         }
     }
-
+    if (std::find(dates.begin(), dates.end(), endate) == dates.end()){
+        dates.push_back(endate);
+    }
+    //Add things to bar and pie chart model
     model->insertRows(model->rowCount(), rowData);
-
     for(int j=0; j<rowData; j++){
         model->setData(j, 0,WString(typeexpenses.at(j)));
         model->setData(j, 1, valueexp.at(j));
+    }
+
+    //Get model for Scatter chart
+    std::sort(dates.begin(), dates.end());
+    scatmodel = new WStandardItemModel(this);
+    scatmodel->insertColumns(scatmodel->columnCount(), 2);
+    scatmodel->setHeaderData(0, WString("Date"));
+    scatmodel->setHeaderData(1, WString("Amount"));
+    double totalspend=0;
+    int siz=dates.size();
+    for(int k=0; k<siz; k++){
+        string curdate=dates.at(k);
+        vector<US_Transaction> onedaytransactions=_user->getMain().getAllTransactions(curdate,curdate);
+        int sizeofday=onedaytransactions.size();
+        for(int l=0; l<sizeofday;l++){
+            totalspend+=onedaytransactions.at(l).getValue();
+        }
+        cout<<"\n";
+        cout<<curdate;
+        cout<<"\n";
+        cout<<totalspend;
+        scatmodel->setData(k, 0,WString(curdate));
+        scatmodel->setData(k, 1, totalspend);
     }
 
     //Add Bar chart
@@ -117,15 +140,10 @@ US_OverviewGUI::US_OverviewGUI(US_Workspace *parent):
     barch->setXSeriesColumn(0);
     Lvbox->addWidget(barch,1);
     series = new Wt::Chart::WDataSeries(1, Wt::Chart::BarSeries,Wt::Chart::Axis::Y1Axis);
-    //for (int column = 1; column < model->columnCount(); ++column) {
-        //series = new Wt::Chart::WDataSeries(column, Wt::Chart::BarSeries,Wt::Chart::Axis::Y1Axis);
-        //Wt::Chart::WDataSeries *series = new Wt::Chart::WDataSeries(column, Wt::Chart::BarSeries,Wt::Chart::Axis::Y1Axis);
-    //}
     barch->addSeries(*series);
     barch->resize(300, 200);
 
     //Add Pie chart
-
     piech = new Wt::Chart::WPieChart(this);
     piech->setModel(model);
     piech->setLabelsColumn(0);    // set the column that holds the labels
@@ -136,26 +154,22 @@ US_OverviewGUI::US_OverviewGUI(US_Workspace *parent):
     piech->setShadowEnabled(true);
     piech->resize(600, 300);
     Rvbox->addWidget(piech);
-
-    /*
+    
     scatch = new Wt::Chart::WCartesianChart(this);
     scatch->setBackground(Wt::WColor(220, 220, 220));
-    scatch->setModel(model);
+    scatch->setModel(scatmodel);
     scatch->setXSeriesColumn(0);
     scatch->setType(Wt::Chart::ScatterPlot);
     scatch->axis(Wt::Chart::XAxis).setScale(Wt::Chart::DateScale);
-    for (int column = 1; column < model->columnCount(); ++column) {
-        Wt::Chart::WDataSeries *s = new Wt::Chart::WDataSeries(column, Wt::Chart::LineSeries);
-        scatch->addSeries(s);
-    }
+    scatseries = new Wt::Chart::WDataSeries(1, Wt::Chart::LineSeries,Wt::Chart::Axis::Y1Axis);
+    scatch->addSeries(*scatseries);
     scatch->resize(500, 300);
-
-    Rvbox->addWidget(scatch,1);*/
+    Rvbox->addWidget(scatch);
 }
 
 void US_OverviewGUI::btnSave_Click() {
-    if(StartDate<EndDate){
-        lblMsg->setText("End Date must be grater than start date");
+    if(StartDate>EndDate){
+        lblMsg->setText("End Date must be greater than start date");
         return;
     }
     string sdate = StartDate->text().toUTF8();
